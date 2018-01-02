a4601ce1ed72a5a9d783c0b80db815cc890b0806
Troels Ugilt Jensen <tujensen@gmail.com>
2018-01-02 13:18:21 +0100

    DOKKWAY-176: Added base app for campaigns

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # campaign-bundle
-Extends os2display with campaigns
+Extends os2display with campaigns.
+
+Campaigns supply a new functionality for controlling content on the screens.
+
+With campaigns a group of screens can be "taken over" for a given period of time
+with a given channel. After the period has expired the screen returns to the
+regular content.
+
+## Installation
+Add the git repository to "repositories" in `composer.json`.
+
+```
+"repositories": {
+    "itk-os2display/campaign-bundle": {
+      "type": "vcs",
+      "url": "https://github.com/itk-os2display/campaign-bundle"
+    },
+    ...
+}
+```
+
+Require the bundle with composer.
+
+```sh
+composer require itk-os2display/campaign-bundle
+```
+
+Enable the bundle in `AppKernel.php`, by adding ItkCampaignBundle to $bundles.
+
+```sh
+new Itk\CampaignBundle\ItkCampaignBundle()
+```
---
+++ Resources/public/apps/itkCampaignApp/itkCampaignApp.js
@@ -0,0 +1,84 @@
+/**
+ * @file
+ * Sets up the Campaign App.
+ */
+
+// Create module and configure routing and translations.
+angular.module('itkCampaignApp').config(['$routeProvider', '$translateProvider', function ($routeProvider, $translateProvider) {
+    'use strict';
+
+    var appUrl = 'bundles/itkcampaign/apps/itkCampaignApp/';
+
+    // Set up translations.
+    $translateProvider
+    .useSanitizeValueStrategy('escape')
+    .useStaticFilesLoader({
+        prefix: appUrl + 'translations/locale-',
+        suffix: '.json'
+    })
+    .preferredLanguage('da')
+    .fallbackLanguage('da')
+    .forceAsyncReload(true);
+
+    // Register routes
+    $routeProvider
+    // Dashboard
+    .when('/campaign', {
+        templateUrl: appUrl + 'campaign-overview.html' + '?' + window.config.version
+    })
+    .when('/campaign/create', {
+        templateUrl: appUrl + 'campaign-create.html' + '?' + window.config.version
+    })
+    .when('/campaign/{id}', {
+        templateUrl: appUrl + 'campaign-edit.html' + '?' + window.config.version
+    });
+}]);
+
+// Setup the app.
+// Register menu items.
+angular.module('itkCampaignApp').service('itkCampaignAppSetup', [
+    'busService', 'userService', '$translate',
+    function (busService, userService, $translate) {
+        'use strict';
+
+        // Register listener for requests for Main Menu items
+        busService.$on('menuApp.requestMainMenuItems', function requestMainMenuItems(event, args) {
+            if (userService.hasRole('ROLE_ADMIN')) {
+                busService.$emit('menuApp.returnMainMenuItems', [
+                    {
+                        title: $translate.instant('menu.campaign'),
+                        route: '/#/campaign',
+                        activeFilter: '/campaign',
+                        icon: 'picture_in_picture',
+                        weight: 6
+                    }
+                ]);
+            }
+        });
+
+        // Listen for sub menu requests
+        busService.$on('menuApp.requestSubMenuItems', function (event, data) {
+            var items = [];
+
+            if (userService.hasRole('ROLE_ADMIN')) {
+                items.push({
+                    title: $translate.instant('menu.campaign_create'),
+                    path: '#/campaign/create',
+                    activeFilter: '/campaign/create',
+                    group: 'left',
+                    weight: 1
+                });
+            }
+
+            busService.$emit('menuApp.returnSubMenuItems', [
+                {
+                    mainMenuItem: 'campaign',
+                    items: items
+                }
+            ]);
+        });
+    }
+]);
+
+// Start the service.
+angular.module('itkCampaignApp').run(['itkCampaignAppSetup', angular.noop]);
