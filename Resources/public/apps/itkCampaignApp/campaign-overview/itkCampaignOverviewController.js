65eed18ab8a2d7e632712723d65d30996222b33b
Troels Ugilt Jensen <tujensen@gmail.com>
2018-01-23 13:25:10 +0100

    DOKKWAY-206: Added delete modal for overview

--- Resources/public/apps/itkCampaignApp/campaign-overview/itkCampaignOverviewController.js
+++ Resources/public/apps/itkCampaignApp/campaign-overview/itkCampaignOverviewController.js
@@ -5,11 +5,16 @@
 
 // @TODO: Cleanup DI.
 angular.module('itkCampaignApp').controller('ItkCampaignOverviewController', [
-    'busService', '$scope', '$timeout', 'ModalService', '$routeParams', '$location', '$controller', '$filter', 'userService',
-    function (busService, $scope, $timeout, ModalService, $routeParams, $location, $controller, $filter, userService) {
+    'busService', '$scope', '$timeout', 'ModalService', '$routeParams', '$location', '$controller', '$filter', 'userService', '$q',
+    function (busService, $scope, $timeout, ModalService, $routeParams, $location, $controller, $filter, userService, $q) {
         'use strict';
 
         $scope.loading = true;
+        $scope.selectedCampaigns = {};
+        $scope.sortOrder = true;
+        $scope.search = {
+            title: ''
+        };
 
         // Extend Os2Display/AdminBundle: BaseApiController.
         $controller('BaseApiController', {$scope: $scope});
@@ -30,45 +35,48 @@ angular.module('itkCampaignApp').controller('ItkCampaignOverviewController', [
             return;
         }
 
-        $scope.search = {
-            title: ''
-        };
+        function refreshCampaignList() {
+            $scope.loading = true;
 
-        $scope.getEntities('campaign').then(
-            function (campaigns) {
-                $scope.campaigns = campaigns;
+            // Load all accessible campaign entities.
+            $scope.getEntities('campaign').then(
+                function (campaigns) {
+                    $scope.campaigns = campaigns;
 
-                var now = parseInt(new Date() / 1000);
+                    var now = parseInt(new Date() / 1000);
 
-                for (var key in $scope.campaigns) {
-                    if ($scope.campaigns.hasOwnProperty(key)) {
-                        var campaign = $scope.campaigns[key];
-                        var schedule_from = parseInt(new Date(campaign.schedule_from) / 1000);
-                        var schedule_to = parseInt(new Date(campaign.schedule_to) / 1000);
+                    for (var key in $scope.campaigns) {
+                        if ($scope.campaigns.hasOwnProperty(key)) {
+                            var campaign = $scope.campaigns[key];
+                            var schedule_from = parseInt(new Date(campaign.schedule_from) / 1000);
+                            var schedule_to = parseInt(new Date(campaign.schedule_to) / 1000);
 
-                        if (schedule_from <= now && schedule_to > now) {
-                            campaign.status = 'active';
-                        }
-                        else {
-                            if (schedule_to < now) {
-                                campaign.status = 'expired';
+                            if (schedule_from <= now && schedule_to > now) {
+                                campaign.status = 'active';
                             }
                             else {
-                                campaign.status = 'future';
+                                if (schedule_to < now) {
+                                    campaign.status = 'expired';
+                                }
+                                else {
+                                    campaign.status = 'future';
+                                }
                             }
                         }
                     }
+                },
+                function (err) {
+                    busService.$emit('log.error', {
+                        cause: err.code,
+                        msg: $translate('common.error.could_not_load_results')
+                    });
                 }
-            },
-            function (err) {
-                busService.$emit('log.error', {
-                    cause: err.code,
-                    msg: $translate('common.error.could_not_load_results')
-                });
-            }
-        ).then(function () {
-            $scope.loading = false;
-        });
+            ).then(function () {
+                $scope.loading = false;
+            });
+        }
+
+        refreshCampaignList();
 
         /**
          * Display help modal.
@@ -86,28 +94,122 @@ angular.module('itkCampaignApp').controller('ItkCampaignOverviewController', [
             });
         };
 
+        /**
+         * Delete selected campaigns.
+         */
         $scope.deleteCampaigns = function () {
-            // Get selected campaigns.
+            var selectedCampaigns = [];
+
+            var campaignCheckboxes = angular.element('.js-campaign-checkbox');
+
+            angular.forEach(campaignCheckboxes, function(element){
+                var id = element.id;
+                var checked = element.checked;
+
+                if (id.indexOf('campaign-') !== -1) {
+                    id = parseInt(id.split('campaign-')[1]);
+
+                    if (checked) {
+                        var campaign = $scope.campaigns.find(function (element) {
+                            return element.id === id;
+                        });
+
+                        if (campaign) {
+                            selectedCampaigns.push(campaign);
+                        }
+                    }
+                }
+            });
 
             // Prompt user for confirm.
+            ModalService.showModal({
+                templateUrl: 'bundles/itkcampaign/apps/itkCampaignApp/campaign-overview/itkCampaignOverviewModalDelete.html',
+                controller: 'ItkCampaignOverviewModalDelete',
+                inputs: {
+                    campaigns: selectedCampaigns
+                }
+            }).then(function (modal) {
+                modal.close.then(function (val) {
+                    if (val === true) {
+                        var promises = [];
 
-            // Delete selected.
+                        // Delete selected.
+                        for (var key in selectedCampaigns) {
+                            if (selectedCampaigns.hasOwnProperty(key)) {
+                                promises.push($scope.deleteEntity('campaign', selectedCampaigns[key]));
+                            }
+                        }
 
-            // Refresh list.
+                        // Refresh list.
+                        $q.all(promises).then(
+                            function () {
+                                refreshCampaignList();
+
+                                busService.$emit('log.info', {
+                                    cause: 200,
+                                    timeout: 3000,
+                                    msg: $translate('messages.campaign_delete_success')
+                                });
+                            },
+                            function (err) {
+                                busService.$emit('log.error', {
+                                    cause: err.code,
+                                    msg: $translate('messages.campaign_delete_error')
+                                });
+                            }
+                        );
+                    }
+
+                    busService.$emit('bodyService.removeClass', 'is-locked');
+                });
+            });
         };
 
+        /**
+         * Flip sort order.
+         */
         $scope.flipSortOrder = function () {
-            // Flip sort order.
-
-            // Make arrow go up/down.
+            $scope.sortOrder = !$scope.sortOrder;
         };
 
+        /**
+         * If less than all checkboxes are selected, select all.
+         * Else select none.
+         */
         $scope.clickAllCheckbox = function () {
-            // Find all checkboxes.
+            var campaignCheckboxes = angular.element('.js-campaign-checkbox');
+            var checkedCampaigns = [];
 
-            // If at least one is filled, deselect all.
+            angular.forEach(campaignCheckboxes, function(element){
+                var id = element.id;
+                var checked = element.checked;
 
-            // Else select all.
+                if (id.indexOf('campaign-') !== -1) {
+                    id = parseInt(id.split('campaign-')[1]);
+
+                    if (checked) {
+                        var campaign = $scope.campaigns.find(function (element) {
+                            return element.id === id;
+                        });
+
+                        if (campaign) {
+                            checkedCampaigns.push(campaign);
+                        }
+                    }
+                }
+            });
+
+            if (checkedCampaigns.length === $scope.campaigns.length) {
+                angular.forEach(campaignCheckboxes, function(element){
+                    element.checked = false;
+                });
+            }
+            else {
+                angular.forEach(campaignCheckboxes, function(element){
+                    element.checked = true;
+                });
+            }
+
         };
     }
 ]);
